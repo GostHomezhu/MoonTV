f2db2ba288711ec12cb92959fa7e03c216ffccaa
llg329 <165636445+llg329@users.noreply.github.com>
2025-09-26 11:22:39 +0800

    Update MobileBottomNav.tsx

--- src/components/MobileBottomNav.tsx
+++ src/components/MobileBottomNav.tsx
@@ -46,8 +46,8 @@ const MobileBottomNav = ({ activePath }: MobileBottomNavProps) => {
     },
     {
       icon: Star,
-      label: '成人VIP区',
-      href: 'https://ccby.cc',
+      label: '豆瓣 Top250',
+      href: '/douban?type=movie&tag=top250&title=豆瓣 Top250',
     },
     {
       icon: Clover,
